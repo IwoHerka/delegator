7bbcd09651af02899b1d641d1162537dad71616d
Siegmeyer of Catarina <hi@iwoherka.eu>
2018-07-10 00:58:52 +0200

    Setup tox

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import delegator
+
+setuptools.setup(
+    name             = 'delegator',
+    version          = delegator.__version__,
+    license          = 'MIT',
+    requires         = ['python (>= 2.7)'],
+    provides         = ['delegator'],
+    description      = 'delegator is a micro-package for defining delegated methods and attributes.',
+    url              = 'http://github.com/IwoHerka/delegator',
+    packages         = setuptools.find_packages(),
+    maintainer       = 'Iwo Herka',
+    maintainer_email = 'hi@iwoherka.eu',
+    author           = 'Iwo Herka',
+    author_email     = 'hi@iwoherka.eu',
+
+    classifiers  = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
