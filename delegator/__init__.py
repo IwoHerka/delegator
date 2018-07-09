600ceca20310c07a20bbae8967b689f04d8d7d17
Siegmeyer of Catarina <hi@iwoherka.eu>
2018-07-09 23:02:41 +0200

    Fill in more docstrings

--- delegator/__init__.py
+++ delegator/__init__.py
@@ -48,7 +48,18 @@ class Delegated(object):
 
 def decorate(cls, src: str = None, attrs: Sequence[str] = None) -> Type:
     """
-    This is where the class is actually modified.
+    This is where the class is actually modified. Each delegated attribute is
+    subsituted on the owner class with attribute accessor object (see
+    `Delegated`). By default, `decorate` expects attribute and delegatee
+    to be specified in the arguments. If arguments are not given, method checks
+    for "delegate" attribute on the class and tries to read missing arguments
+    from its value. "delegate" can be a sequence or a single string with
+    arguments separated by whitespace (similar to `namedtuple` syntax).
+    Delegatee must always be the first argument. For example:
+
+        delegate = 'attr spam ham eggs'
+
+    where `spam`, `ham` and `eggs` are some attributes on `cls.<attr>`.
     """
     if not (src and attrs) and hasattr(cls, 'delegate'):
         if isinstance(cls.delegate, tuple):
@@ -70,10 +81,25 @@ def decorate(cls, src: str = None, attrs: Sequence[str] = None) -> Type:
 
 
 def delegator(cls: Type) -> Type:
+    """
+    Class decorator for the "delegate" string notation.
+    """
     return decorate(cls)
 
 
+class Delegator(type):
+    """
+    Metaclass for the "delegate" string notation.
+    """
+    def __new__(*args: Any, **kwargs: Any):
+        return decorate(type.__new__(*args, **kwargs))
+
+
 class delegate(object):
+    """
+    Class decorator for explicit argument passing. Expects a string or some
+    sequence specifying delegatee and delegated attributes.
+    """
     def __init__(self, src: str, *attrs: str) -> None:
         self.src: str
         self.attrs: Sequence[str]
@@ -91,8 +117,3 @@ class delegate(object):
 
     def __call__(self, cls: Type) -> Type:
         return decorate(cls, self.src, self.attrs)
-
-
-class Delegator(type):
-    def __new__(*args: Sequence, **kwargs: Sequence):
-        return decorate(type.__new__(*args, **kwargs))
